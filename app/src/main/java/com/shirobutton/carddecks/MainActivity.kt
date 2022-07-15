39fad3130ee046ea5fd727200ca097c0d60e333f
シロボタン <shirobutton@gmail.com>
2022-07-15 15:27:58 +0900

    MainActivityにCardListのRecyclerViewを実装

--- app/src/main/java/com/shirobutton/carddecks/MainActivity.kt
+++ app/src/main/java/com/shirobutton/carddecks/MainActivity.kt
@@ -2,10 +2,29 @@ package com.shirobutton.carddecks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.shirobutton.carddecks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bindRecyclerView(binding)
     }
+
+    private fun bindRecyclerView(binding: ActivityMainBinding) {
+        val adapter = CardListAdapter()
+        adapter.submitList(createData())
+        binding.root.adapter = adapter
+        binding.root.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun createData() =
+        List(3) {
+            CardListItem(
+                title = it.toString(),
+                imageUrl = "https://placehold.jp/100x100.png"
+            )
+        }
 }
